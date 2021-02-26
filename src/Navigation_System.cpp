3d1bb75106ba00ea251a1a8441468289947e1506
Reza <reza_alavi63@yahoo.com>
2021-02-26 17:01:21 +0100

    Initialization of the project

---
+++ src/Navigation_System.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Navigation_System.cpp
+// Author      : Sayeed Reza Alawi
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
